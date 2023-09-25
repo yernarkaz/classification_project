ddfa974d22503469478dd32f54a28affb028ffe5
Yernar <yernarkaz@gmail.com>
2023-09-26 00:30:12 +0600

    Complete the process of reproducing ML pipeline stages.

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,6 +5,8 @@ import numpy as np
 import pickle
 
 import dvc.api
+import mlflow
+import mlflow.sklearn
 from pathlib import Path
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
@@ -16,9 +18,10 @@ warnings.filterwarnings("ignore")
 
 
 
-def inference_and_evaluate(params, model_pipeline, test_data, tags, model_name):
+def inference_and_evaluate(experiment, params, model_pipeline, test_data, tags, model_name):
     """
     Evaluate the model pipeline on the test set
+    :param experiment: evalution experiment
     :param params: params for model training
     :param model_pipeline: the model pipeline to evaluate
     :param test_data: the test data
@@ -28,19 +31,30 @@ def inference_and_evaluate(params, model_pipeline, test_data, tags, model_name):
     """
     
     result_list = []
+    
     for tag in tqdm(tags):
-        
-        test = test_data[tag]
-        X_test, y_test = np.array([vec for vec in test.sentence_embeddings.values]), test[tag]
-        y_pred = model_pipeline.predict(X_test)
-        
-        result_list.append({
-            "model_name": model_name,
-            "tag": tag,
-            "f1_score": f1_score(y_test, y_pred),
-            "precision": precision_score(y_test, y_pred),
-            "recall": recall_score(y_test, y_pred)
-        })
+        with mlflow.start_run(experiment_id=experiment.experiment_id, run_name=f"tag: {str(tag)}", nested=True):
+
+            test = test_data[tag]
+            X_test, y_test = np.array([vec for vec in test.sentence_embeddings.values]), test[tag]
+            y_pred = model_pipeline.predict(X_test)
+
+            f1__score = f1_score(y_test, y_pred)
+            pr_score = precision_score(y_test, y_pred)
+            rec_score = recall_score(y_test, y_pred)
+
+            mlflow.log_metric("f1_score", f1__score)
+            mlflow.log_metric("precision_score", pr_score)
+            mlflow.log_metric("recall_score", rec_score)
+            
+            result_list.append({
+                "account_ids": ','.join(params["account_ids"]),
+                "model_name": model_name,
+                "tag": tag,
+                "f1_score": f1__score,
+                "precision": pr_score,
+                "recall": rec_score
+            })
 
     return pd.DataFrame(result_list)
 
@@ -65,6 +79,7 @@ def save_results(repo_path, params, results):
     """
 
     results.to_csv(repo_path / params['result_path'], index=False)
+    results.to_csv(repo_path / params['result_path_track'], index=False)
 
 
 def main(repo_path):
@@ -90,12 +105,26 @@ def main(repo_path):
     print("Loading model pipelines...")
     model_pipelines = load_model(repo_path, params)
 
+    experiment = mlflow.set_experiment(params["experiment_name"])
+
     print("Inference model pipelines...")
     result_list = []
+
     for model_name in params["models"]:
-        result_list.append(
-            inference_and_evaluate(params, model_pipelines[model_name], test_data, tags, model_name)
-        )
+        print("Inference model:", model_name)
+        with mlflow.start_run(experiment_id=experiment.experiment_id, run_name=f"Evaluation of {model_name}"):
+
+            mlflow.log_param("seed", params["seed"])
+            mlflow.log_param("data_version", params["data_version"])
+            mlflow.log_param("account_ids", params["account_ids"])
+            mlflow.log_param("embedding_pooling_type", params["embedding_pooling_type"])
+            mlflow.log_param("preprocess_type", params["preprocess_type"])
+            mlflow.log_param("model", model_name)
+            mlflow.log_param("pca_n_components", params["pca_n_components"])
+            
+            result_list.append(
+                inference_and_evaluate(experiment, params, model_pipelines[model_name], test_data, tags, model_name)
+            )
 
     print("Saving results...")
     save_results(repo_path, params, pd.concat(result_list))
--- src/train.py
+++ src/train.py
@@ -5,6 +5,8 @@ import numpy as np
 import pickle
 
 import dvc.api
+import mlflow
+import mlflow.sklearn
 from pathlib import Path
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
@@ -46,7 +48,7 @@ def train_model_pipeline(params, model, train_data, tags):
         ('clf', model)
     ])
     
-    for tag in tqdm(tags[:5]):
+    for tag in tqdm(tags):
         train = train_data[tag]
         X_train, y_train = np.array([vec for vec in train.sentence_embeddings.values]), train[tag]  
         model_pipeline.fit(X_train, y_train)
@@ -74,9 +76,12 @@ def main(repo_path):
     """
 
     print("repo_path:", repo_path)
-    params = dvc.api.params_show(stages="train")
+    params = dvc.api.params_show(stages=["preprocess", "train"])
     print("params:")
     print(params)
+
+    print("Seeding random...")
+    np.random.seed(params["seed"])
 
     print("Loading tags...")
     tags = np.load(repo_path / params["tags_path"])
@@ -86,11 +91,27 @@ def main(repo_path):
     for tag in tqdm(tags):
         train_data[tag] = pd.read_parquet(repo_path / params["train_path"] / f"{tag.replace('/', '_')}")
 
-    print("Training model pipelines...")
-    model_pipelines = {}
-    for model_name in params["models"]:
-        model = get_model(model_name)
-        model_pipelines[model_name] = train_model_pipeline(params, model, train_data, tags)
+    
+    experiment = mlflow.set_experiment(params["experiment_name"])
+
+    with mlflow.start_run(experiment_id=experiment.experiment_id, run_name="training"):
+        mlflow.log_param("seed", params["seed"])
+        mlflow.log_param("data_version", params["data_version"])
+        mlflow.log_param("account_ids", params["account_ids"])
+        mlflow.log_param("embedding_pooling_type", params["embedding_pooling_type"])
+        mlflow.log_param("preprocess_type", params["preprocess_type"])
+
+        print("Training model pipelines...")
+        model_pipelines = {}
+        for model_name in params["models"]:
+
+            print("Training model:", model_name)
+            with mlflow.start_run(experiment_id=experiment.experiment_id, run_name=model_name, nested=True):
+                mlflow.log_param("model", model_name)
+                mlflow.log_param("pca_n_components", params["pca_n_components"])
+
+                model = get_model(model_name)
+                model_pipelines[model_name] = train_model_pipeline(params, model, train_data, tags)
 
     print("Saving model pipelines...")
     save_model(repo_path, params, model_pipelines)
